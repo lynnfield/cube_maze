c26d77972906e3f463906d3cdf300b75d0c1308b
lynnfield <genovich.v.v@gmail.com>
2016-06-02 11:39:22 +0300

    FPV camera, roll

--- js/Player.js
+++ js/Player.js
@@ -1,124 +1,153 @@
-
 function Player(cell) {
-  this.cell = cell;
-  this.movements = 0;
-  this.successMovements = 0;
-  
-  return this;
+    this.cell = cell;
+    this.movements = 0;
+    this.successMovements = 0;
+
+    return this;
 }
 
 function isAllowedToMove(cell) {
-  return cell && cell.isEmpty();
+    return cell && cell.isEmpty();
 }
 
-Player.prototype._move = function(cell) {
-  if (isAllowedToMove(cell)) {
-    this.cell = cell;
-    this.successMovements++;
-  }
-  this.movements++;
+Player.prototype._move = function (cell) {
+    if (isAllowedToMove(cell)) {
+        this.cell = cell;
+        this.successMovements++;
+    }
+    this.movements++;
 };
 
-Player.prototype.getPosition = function() {
-  return this.cell;
+Player.prototype.getPosition = function () {
+    return this.cell;
 };
 
-Player.prototype.moveUp = function() {
-  this._move(this.cell.getUp());
+Player.prototype.moveUp = function () {
+    this._move(this.cell.getUp());
 };
 
-Player.prototype.moveDown = function() {
-  this._move(this.cell.getDown());
+Player.prototype.moveDown = function () {
+    this._move(this.cell.getDown());
 };
 
-Player.prototype.moveRight = function() {
-  this._move(this.cell.getRight());
+Player.prototype.moveRight = function () {
+    this._move(this.cell.getRight());
 };
 
-Player.prototype.moveLeft = function() {
-  this._move(this.cell.getLeft());
+Player.prototype.moveLeft = function () {
+    this._move(this.cell.getLeft());
 };
 
-Player.prototype.moveForward = function() {
-  this._move(this.cell.getForward());
+Player.prototype.moveForward = function () {
+    this._move(this.cell.getForward());
 };
 
-Player.prototype.moveBackward = function() {
-  this._move(this.cell.getBackward());
+Player.prototype.moveBackward = function () {
+    this._move(this.cell.getBackward());
 };
 
-Player.prototype.getUp = function(cell, num) {
-  return cell.getUp(num);
+Player.prototype.getUp = function (cell, num) {
+    return cell.getUp(num);
 };
 
-Player.prototype.getDown = function(cell, num) {
-  return cell.getDown(num);
+Player.prototype.getDown = function (cell, num) {
+    return cell.getDown(num);
 };
 
-Player.prototype.getRight = function(cell, num) {
-  return cell.getRight(num);
+Player.prototype.getRight = function (cell, num) {
+    return cell.getRight(num);
 };
 
-Player.prototype.getLeft = function(cell, num) {
-  return cell.getLeft(num);
+Player.prototype.getLeft = function (cell, num) {
+    return cell.getLeft(num);
 };
 
-Player.prototype.getForward = function(cell, num) {
-  return cell.getForward(num);
+Player.prototype.getForward = function (cell, num) {
+    return cell.getForward(num);
 };
 
-Player.prototype.getBackward = function(cell, num) {
-  return cell.getBackward(num);
+Player.prototype.getBackward = function (cell, num) {
+    return cell.getBackward(num);
 };
 
-Player.prototype.rotateRight = function() {
-  var move = this.moveUp;
-  this.moveUp = this.moveRight;
-  this.moveRight = this.moveDown;
-  this.moveDown = this.moveLeft;
-  this.moveLeft = move;
-  var direction = this.getUp;
-  this.getUp = this.getRight;
-  this.getRight = this.getDown;
-  this.getDown = this.getLeft;
-  this.getLeft = direction;
+Player.prototype.rotateRight = function () {
+    var move = this.moveUp;
+    this.moveUp = this.moveRight;
+    this.moveRight = this.moveDown;
+    this.moveDown = this.moveLeft;
+    this.moveLeft = move;
+    var direction = this.getUp;
+    this.getUp = this.getRight;
+    this.getRight = this.getDown;
+    this.getDown = this.getLeft;
+    this.getLeft = direction;
 };
 
-Player.prototype.rotateLeft = function() {
-  var move = this.moveUp;
-  this.moveUp = this.moveLeft;
-  this.moveLeft = this.moveDown;
-  this.moveDown = this.moveRight;
-  this.moveRight = move;
-  var direction = this.getUp;
-  this.getUp = this.getLeft;
-  this.getLeft = this.getDown;
-  this.getDown = this.getRight;
-  this.getRight = direction;
+Player.prototype.rotateLeft = function () {
+    var move = this.moveUp;
+    this.moveUp = this.moveLeft;
+    this.moveLeft = this.moveDown;
+    this.moveDown = this.moveRight;
+    this.moveRight = move;
+    var direction = this.getUp;
+    this.getUp = this.getLeft;
+    this.getLeft = this.getDown;
+    this.getDown = this.getRight;
+    this.getRight = direction;
 };
 
-Player.prototype.rotateForward = function() {
-  var move = this.moveForward;
-  this.moveForward = this.moveDown;
-  this.moveDown = this.moveBackward;
-  this.moveBackward = this.moveUp;
-  this.moveUp = move;
-  var direction = this.getForward;
-  this.getForward = this.getDown;
-  this.getDown = this.getBackward;
-  this.getBackward = this.getUp;
-  this.getUp = direction;
+Player.prototype.rotateForward = function () {
+    var move = this.moveForward;
+    this.moveForward = this.moveDown;
+    this.moveDown = this.moveBackward;
+    this.moveBackward = this.moveUp;
+    this.moveUp = move;
+
+    var direction = this.getForward;
+    this.getForward = this.getDown;
+    this.getDown = this.getBackward;
+    this.getBackward = this.getUp;
+    this.getUp = direction;
 };
 
-Player.prototype.rotateBackward = function() {
-  var move = this.moveForward;
-  this.moveForward = this.moveUp;
-  this.moveUp = this.moveBackward;
-  this.moveBackward = this.moveDown;
-  this.moveDown = move;
-  var direction = this.getForward;
-  this.getForward = this.getUp;
-  this.getUp = this.getBackward;
-  this.getBackward = this.getDown;
-  this.getDown = direction;
+Player.prototype.rotateBackward = function () {
+    var move = this.moveForward;
+    this.moveForward = this.moveUp;
+    this.moveUp = this.moveBackward;
+    this.moveBackward = this.moveDown;
+    this.moveDown = move;
+
+    var direction = this.getForward;
+    this.getForward = this.getUp;
+    this.getUp = this.getBackward;
+    this.getBackward = this.getDown;
+    this.getDown = direction;
+};
+
+Player.prototype.rollLeft = function () {
+    var move = this.moveForward;
+    this.moveForward = this.moveRight;
+    this.moveRight = this.moveBackward;
+    this.moveBackward = this.moveLeft;
+    this.moveLeft = move;
+
+    var direction = this.getForward;
+    this.getForward = this.getRight;
+    this.getRight = this.getBackward;
+    this.getBackward = this.getLeft;
+    this.getLeft = direction;
+};
+
+Player.prototype.rollRight = function () {
+    var move = this.moveForward;
+    this.moveForward = this.moveLeft;
+    this.moveLeft = this.moveBackward;
+    this.moveBackward = this.moveRight;
+    this.moveRight = move;
+
+    var direction = this.getForward;
+    this.getForward = this.getLeft;
+    this.getLeft = this.getBackward;
+    this.getBackward = this.getRight;
+    this.getRight = direction;
 };
--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 1, 1000);
+var camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
+var xAxis = new THREE.Vector3(1, 0, 0);
+var zAxis = new THREE.Vector3(0, 0, 1);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight - 4);
@@ -9,18 +11,19 @@ document.body.appendChild(renderer.domElement);
 var ambientLight = new THREE.AmbientLight(0x0a0a0a); // soft white light
 scene.add(ambientLight);
 
-// generate maze
+// parse maze size
 var paramsParser = new ParamsParser({
     defaultX: 5,
     defaultY: 5,
     defaultZ: 5
 });
 
-console.log('create maze ' + paramsParser.sizeX + 'x' + paramsParser.sizeY + 'x' + paramsParser.sizeZ);
+// generate maze
 var mazeCreator = new MazeCreator(paramsParser.sizeX, paramsParser.sizeY, paramsParser.sizeZ);
 mazeCreator.generate();
 
-var standardBoxGeometry = new THREE.BoxGeometry(1,1,1);
+// add start & end boxes
+var standardBoxGeometry = new THREE.BoxGeometry(1, 1, 1);
 var startMesh = new THREE.Mesh(
     standardBoxGeometry,
     new THREE.MeshStandardMaterial({
@@ -38,20 +41,28 @@ var endMesh = new THREE.Mesh(
 startMesh.position.copy(mazeCreator.start);
 endMesh.position.copy(mazeCreator.end);
 scene.add(startMesh, endMesh);
-// end generate maze
 
-
+// create lights
 var light = new THREE.DirectionalLight(0xffffff, 0.3);
 var pointLight = new THREE.PointLight(0xffffff, 1, 5);
+
+
+var thirdPersonViewCameraPosition = new THREE.Object3D();
+var firstPersonViewCameraPosition = new THREE.Object3D();
 
 // generate and place player
 var player = new Player(mazeCreator.start);
 var playerSphere = placeSphereAt(player.getPosition());
-playerSphere.add(camera, light, pointLight);
+playerSphere.add(camera, light, pointLight, firstPersonViewCameraPosition, thirdPersonViewCameraPosition);
 
+// configure lights
 light.target = playerSphere;
 light.translateZ(2);
-camera.translateZ(5);
+
+// configure camera
+firstPersonViewCameraPosition.rotateOnAxis(xAxis, Math.PI / 2);
+thirdPersonViewCameraPosition.translateZ(5);
+var currentCameraPosition = thirdPersonViewCameraPosition;
 
 // draw
 var drawer = new Drawer(scene);
@@ -60,20 +71,24 @@ drawer.draw(player);
 var targetRotation = new THREE.Object3D();
 
 // update
+var reloading = false;
 function updatePlayer(deltaTime) {
     deltaTime = deltaTime || 1;
     playerSphere.position.lerp(player.getPosition(), 0.01 * deltaTime);
     playerSphere.quaternion.slerp(targetRotation.quaternion, 0.01 * deltaTime);
 
+    camera.position.lerp(currentCameraPosition.position, 0.01 * deltaTime);
+    camera.quaternion.slerp(currentCameraPosition.quaternion, 0.01 * deltaTime);
+
     if (player.cell == mazeCreator.end) {
-        console.log('win');
-        window.location.reload();
+        if (!reloading) {
+            reloading = true;
+            console.log('win');
+            window.location.reload();
+        }
     }
 }
 updatePlayer();
-
-var xAxis = new THREE.Vector3(1, 0, 0);
-var zAxis = new THREE.Vector3(0, 0, 1);
 
 // movement
 addEventListener('keyup', function (event) {
@@ -92,13 +107,27 @@ addEventListener('keyup', function (event) {
         case 40:  // down
             player.moveDown();
             break;
-        case 87:  // forward 'a'
+        case 87:  // forward 'w'
             player.rotateForward();
             targetRotation.rotateOnAxis(xAxis, -Math.PI / 2);
             break;
-        case 83:  // backward 'b'
+        case 83:  // backward 's'
             player.rotateBackward();
             targetRotation.rotateOnAxis(xAxis, Math.PI / 2);
+            break;
+        case 65: // roll left 'a'
+            player.rollLeft();
+            targetRotation.rotateOnAxis(targetRotation.up, -Math.PI / 2);
+            break;
+        case 68: // roll right 'd'
+            player.rollRight();
+            targetRotation.rotateOnAxis(targetRotation.up, Math.PI / 2);
+            break;
+        case 67: // camera 'c'
+            if (currentCameraPosition == firstPersonViewCameraPosition)
+                currentCameraPosition = thirdPersonViewCameraPosition;
+            else
+                currentCameraPosition = firstPersonViewCameraPosition;
             break;
     }
 
@@ -106,8 +135,6 @@ addEventListener('keyup', function (event) {
 
     var from = mazeCreator.end;
     var to = player.cell;
-
-    console.log('distance to end: ' + Math.floor(from.distanceTo(to)) + ' movements: ' + player.successMovements + '/' + player.movements);
 
     document.querySelector('#distanceToEnd').textContent = 'Distance to end: ' + Math.floor(from.distanceTo(to));
 });
